f3fc78e840f31b50882aafa1416f31272066d5da
Bebomonky <bebomonky@gmail.com>
2024-06-25 16:46:51 -0400

    Epic loop table buttons for builderMenu

--- CED.rte/Actors/Shared/Scripts/BuilderMenu.lua
+++ CED.rte/Actors/Shared/Scripts/BuilderMenu.lua
@@ -12,12 +12,7 @@ function Create(self)
 	self.ConfirmSound = CreateSoundContainer("Base.rte/Confirm")
 	self.PieMenu:AddPieSlice(CreatePieSlice("CED.rte/BuilderMenu"), self)
 
-	self.SelectedObject = false
 	self.SelectDelayTime = Timer()
-
-	self.Icons = {
-		Drone = "Coalition.rte/Actors/Mecha/GatlingDrone/Icon.png"
-	}
 end
 
 function BuilderBasic(self)
@@ -30,59 +25,50 @@ function BuilderBasic(self)
 	self.Main.Box:SetOutlineColor(71)
 	self.Main.Box:SetOutlineThickness(2)
 
-	local button = igui.Button()
-	button:SetName("Gatling_Turret")
-	button:SetParent(self.Main.Box)
-	button:SetPos(Vector(10, 15))
-	button:SetSize(Vector(28, 26))
-	button:SetColor(146)
-	button:SetOutlineThickness(2)
-	button:SetOutlineColor(144)
+	local rows = 4
+	for i = 1, #self.CEDAvailableBuildables do
+		local buildable = self.CEDAvailableBuildables[i]
+		buildable.Selected = false
 
-	button.Think = function(entity, screen)
-		button:SetOutlineColor(button.IsHovered and 117 or 144)
-		local world_pos = button.Parent.Pos + button:GetPos() + CameraMan:GetOffset(screen)
-		PrimitiveMan:DrawBitmapPrimitive(screen, world_pos + Vector(15, 13), self.Icons.Drone, 0)
+		local x = -5 + self.Main.Box:GetPos().X + ((i - 1) % rows + 1 - 1) * 60
+		local y = 7 + (math.floor((i - 1) / rows ) + 1 - 1) * 35
 
-		if self.SelectedObject then
+		local button = igui.Button()
+		button:SetName("Buildable " .. i)
+		button:SetParent(self.Main.Box)
+		button:SetPos(Vector(x, y))
+		button:SetSize(Vector(49, 26))
+		button:SetColor(146)
+		button:SetText(buildable.DisplayName)
+		button:SetTextPos(Vector(0, 10))
+		button:SetOutlineThickness(2)
+		button:SetOutlineColor(144)
 
-			PrimitiveMan:DrawBitmapPrimitive(screen, self.Menu.Cursor, self.Icons.Drone, 0)
-			if self.SelectDelayTime:IsPastSimMS(200) then
-				if self.Menu.Controller:IsState(Controller.PRIMARY_ACTION) then
-					local buildable = CreateMOSRotating("CED.rte/CED Generic Buildable")
-					buildable.Team = entity.Team
-					buildable.Pos = self.Menu.Cursor
-					MovableMan:AddParticle(buildable)
-					self.SelectDelayTime:Reset()
-					self.SelectedObject = false
+		button.Think = function(entity, screen)
+			button:SetOutlineColor(button.IsHovered and 117 or 144)
+			local world_pos = button.Parent.Pos + button:GetPos() + CameraMan:GetOffset(screen)
+			PrimitiveMan:DrawBitmapPrimitive(screen, world_pos + Vector(15, 13), buildable.IconPath, 0)
+	
+			if buildable.Selected then
+				PrimitiveMan:DrawBitmapPrimitive(screen, self.Menu.Cursor, buildable.IconPath, 0)
+				if self.SelectDelayTime:IsPastSimMS(200) then
+					if self.Menu.Controller and self.Menu.Controller:IsState(Controller.PRIMARY_ACTION) then
+						local buildable = CreateMOSRotating("CED.rte/CED Generic Buildable")
+						buildable.Team = entity.Team
+						buildable.Pos = self.Menu.Cursor
+						MovableMan:AddParticle(buildable)
+						self.SelectDelayTime:Reset()
+						buildable.Selected = false
+					end
 				end
 			end
 		end
-	end
-
-	button.OnPress = function(key)
-		if key == Controller.PRIMARY_ACTION then
-			self.SelectDelayTime:Reset()
-			self.SelectedObject = true
-		end
-	end
-
-	local cancelButton = igui.Button()
-	cancelButton:SetName("CancelButton")
-	cancelButton:SetParent(self.Main.Box)
-	cancelButton:SetPos(Vector(50, 15))
-	cancelButton:SetSize(Vector(26, 26))
-	cancelButton:SetColor(146)
-	cancelButton:SetOutlineThickness(2)
-	cancelButton:SetOutlineColor(144)
-
-	cancelButton.Think = function(entity, screen)
-		cancelButton:SetOutlineColor(cancelButton.IsHovered and 117 or 144)
-	end
-
-	cancelButton.OnPress = function(key)
-		if key == Controller.PRIMARY_ACTION then
-			print("bbbbb")
+	
+		button.OnPress = function(key)
+			if key == Controller.PRIMARY_ACTION then
+				self.SelectDelayTime:Reset()
+				buildable.Selected = true
+			end
 		end
 	end
 
