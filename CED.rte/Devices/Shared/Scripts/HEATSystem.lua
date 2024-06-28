0b451061e6bd744079d0fd8cd5d9a4629e8ee903
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2024-06-28 13:28:33 +0300

    Test pistol (successful)

---
+++ CED.rte/Devices/Shared/Scripts/HEATSystem.lua
@@ -0,0 +1,402 @@
+function Create(self)
+	self.HEATParent = nil;
+	self.HEATParentSet = false;
+	
+	self.HEATLastAge = self.Age
+	
+	self.HEATOriginalSharpLength = self.SharpLength
+	self.HEATOriginalStanceOffset = Vector(math.abs(self.StanceOffset.X), self.StanceOffset.Y)
+	self.HEATOriginalSharpStanceOffset = Vector(self.SharpStanceOffset.X, self.SharpStanceOffset.Y)
+	self.HEATOriginalSupportOffset = Vector(self.SupportOffset.X, self.SupportOffset.Y)
+	
+	self.HEATRotation = 0
+	self.HEATRotationTarget = 0
+	self.HEATRotationSpeed = 9
+	self.HEATHorizontalAnim = 0
+	self.HEATVerticalAnim = 0
+	self.HEATAngVel = 0
+	self.HEATLastRotAngle = self.RotAngle
+	self.HEATLastHFlipped = self.HFlipped
+	
+	self.HEATReloadStanceOffset = Vector(0, 0);
+	self.HEATReloadStanceOffsetTarget = Vector(0, 0)
+	self.HEATReloadSupportOffsetSpeed = 20
+	self.HEATReloadSupportOffsetTarget = Vector(0, 0);
+	
+	self.HEATReloadTimer = Timer();
+	self.HEATCurrentReloadPhase = 1;
+	
+	self.BaseReloadTime = 19999;
+	
+	self.HEATFireDelayTimer = Timer();
+	self.HEATDelayedFire = false
+	self.HEATDelayedFireTimer = Timer();
+	self.HEATDelayedFireActivated = false
+	
+	self.HEATFiringAnimationTimer = Timer();
+end
+
+function Update(self)
+	self.Frame = 0;
+	self.HEATRotationTarget = 0
+	
+	if self.ID == self.RootID then
+		self.HEATParent = nil;
+		self.HEATParentSet = false;
+	elseif self.HEATParentSet == false then
+		local actor = MovableMan:GetMOFromID(self.RootID);
+		if actor and IsAHuman(actor) then
+			self.HEATParent = ToAHuman(actor);
+			self.HEATParentSet = true;
+		end
+	end
+	
+    -- Smoothing
+    local min_value = -math.pi;
+    local max_value = math.pi;
+    local value = self.RotAngle - self.HEATLastRotAngle
+    local result;
+    local ret = 0
+    
+    local range = max_value - min_value;
+    if range <= 0 then
+        result = min_value;
+    else
+        ret = (value - min_value) % range;
+        if ret < 0 then ret = ret + range end
+        result = ret + min_value;
+    end
+    
+    self.HEATLastRotAngle = self.RotAngle
+    self.HEATAngVel = (result / TimerMan.DeltaTimeSecs) * self.FlipFactor
+    
+    if self.HEATLastHFlipped ~= nil then
+        if self.HEATLastHFlipped ~= self.HFlipped then
+            self.HEATLastHFlipped = self.HFlipped
+            self.HEATAngVel = 0
+        end
+    end
+	
+	-- Check if switched weapons/hide in the inventory, etc.
+	if self.Age > (self.HEATLastAge + TimerMan.DeltaTimeSecs * 2000) then
+		if self.HEATDelayedFire then
+			self.HEATDelayedFire = false
+		end
+		self.HEATFireDelayTimer:Reset()
+	end
+	self.HEATLastAge = self.Age + 0
+	
+	-- SLIDE animation when firing
+	-- don't ask, math magic
+	local f = math.max(1 - math.min((self.HEATFiringAnimationTimer.ElapsedSimTimeMS) / 200, 1), 0)
+	self.Frame = math.floor(f * 3 + 0.55);
+	
+	-- Reload system
+
+	if self:IsReloading() then
+
+		self.HEATFireDelayTimer:Reset()
+		self.HEATDelayedFireActivated = false;
+		self.HEATDelayedFire = false;
+		
+		if not self.HEATCurrentReloadPhaseData then
+			self.HEATCurrentReloadPhaseData = {};
+			-- To support things in callbacks overriding values without overriding originals, we need a table copy:
+			for k, v in pairs(self.HEATReloadPhases[self.HEATCurrentReloadPhase]) do
+				self.HEATCurrentReloadPhaseData[k] = v;
+			end	
+		end
+		
+		self.HEATReloadPhaseOnInterrupt = self.HEATCurrentReloadPhaseData.phaseOnInterrupt or self.HEATCurrentReloadPhase;
+		
+		if self.HEATEnterPhaseCallbackDone ~= true then
+			self.HEATEnterPhaseCallbackDone = true;
+			self.HEATCurrentReloadPhaseData.enterPhaseCallback(self);
+		end
+		
+		if self.HEATReloadTimer:IsPastSimMS(self.HEATCurrentReloadPhaseData.prepareDelay - self.HEATCurrentReloadPhaseData.prepareSoundLength) and self.HEATPrepareSoundPlayed ~= true then
+			self.HEATPrepareSoundPlayed = true;
+			if self.HEATCurrentReloadPhaseData.prepareSound then
+				self.HEATCurrentReloadPhaseData.prepareSound:Play(self.Pos)
+			end
+		end
+		
+		self.Frame = self.HEATCurrentReloadPhaseData.startFrame;
+		self.HEATRotationTarget = self.HEATCurrentReloadPhaseData.rotationTarget;
+		
+		self.HEATReloadStanceOffsetTarget = self.HEATCurrentReloadPhaseData.reloadStanceOffsetTarget;
+		self.HEATReloadSupportOffsetSpeed = self.HEATCurrentReloadPhaseData.reloadSupportOffsetSpeed;
+		self.HEATReloadSupportOffsetTarget = self.HEATCurrentReloadPhaseData.reloadSupportOffsetTarget;
+		
+		self.HEATCurrentReloadPhaseData.constantCallback(self);
+	
+		if self.HEATReloadTimer:IsPastSimMS(self.HEATCurrentReloadPhaseData.prepareDelay) then
+		
+			-- Frame animation
+			local progressFactor = (self.HEATReloadTimer.ElapsedSimTimeMS - self.HEATCurrentReloadPhaseData.prepareDelay) / self.HEATCurrentReloadPhaseData.afterDelay
+			if progressFactor > 1 then
+				progressFactor = 1;
+			end			
+			
+			local frameChange = self.HEATCurrentReloadPhaseData.endFrame - self.HEATCurrentReloadPhaseData.startFrame
+			self.Frame = math.floor(self.HEATCurrentReloadPhaseData.startFrame + math.floor(frameChange * progressFactor, 0.55))
+			
+			if self.HEATAfterSoundPlayed ~= true then
+			
+				if self.HEATCurrentReloadPhaseData.removesMag then
+					self:SetNumberValue("HEAT_FakeMagRemoved", 1);
+					local fakeMag
+					fakeMag = self.HEATFakeMagazineMOSRotating:Clone();
+					fakeMag.Pos = self.Pos + Vector(self.HEATFakeMagazineOffset.X * self.FlipFactor, self.HEATFakeMagazineOffset.Y):RadRotate(self.RotAngle);
+					fakeMag.Vel = self.Vel + Vector(self.HEATFakeMagazineVelocity.X * self.FlipFactor, self.HEATFakeMagazineVelocity.Y):RadRotate(self.RotAngle);
+					fakeMag.RotAngle = self.RotAngle;
+					fakeMag.AngularVel = self.HEATFakeMagazineAngularVel * self.FlipFactor;
+					fakeMag.HFlipped = self.HFlipped;
+					MovableMan:AddParticle(fakeMag);
+				elseif self.HEATCurrentReloadPhaseData.addsMag then
+					self:RemoveNumberValue("HEAT_FakeMagRemoved");
+				end
+				
+				self.HEATAngVel = self.HEATAngVel + self.HEATCurrentReloadPhaseData.angVel;
+				self.HEATHorizontalAnim = self.HEATHorizontalAnim + self.HEATCurrentReloadPhaseData.horizontalAnim;
+				self.HEATVerticalAnim = self.HEATVerticalAnim + self.HEATCurrentReloadPhaseData.verticalAnim;
+			
+				self.HEATAfterSoundPlayed = true;
+				if self.HEATCurrentReloadPhaseData.afterSound then
+					self.HEATCurrentReloadPhaseData.afterSound:Play(self.Pos);
+				end
+				
+				self.HEATCurrentReloadPhaseData.finishCallback(self);
+				
+			end
+			
+			if self.HEATReloadTimer:IsPastSimMS(self.HEATCurrentReloadPhaseData.prepareDelay + self.HEATCurrentReloadPhaseData.afterDelay) then
+				self.HEATReloadTimer:Reset();
+				self.HEATPrepareSoundPlayed = false;
+				self.HEATAfterSoundPlayed = false;
+				
+				self.HEATReloadPhaseOnInterrupt = nil;
+				
+				if not self.HEATEmptyReload and self.HEATCurrentReloadPhaseData.endIfNotEmptyReload then
+					self.HEATCurrentReloadPhase = 1;
+					self.HEATReloadStanceOffsetTarget = Vector(0, 0);
+					self.HEATReloadSupportOffsetTarget = Vector(0, 0);
+					
+					self.HEATReloadPhaseOverride = nil;
+					
+					self.HEATEnterPhaseCallbackDone = true;
+					self.HEATExitPhaseCallbackDone = true;
+					
+					self.BaseReloadTime = 0;
+				elseif self.HEATReloadPhaseOverride then
+					self.HEATCurrentReloadPhase = self.HEATReloadPhaseOverride;
+					self.HEATReloadPhaseOverride = nil;
+				elseif self.HEATReloadPhases[self.HEATCurrentReloadPhase + 1] == nil then
+					self.HEATCurrentReloadPhase = 1;
+					self.HEATReloadStanceOffsetTarget = Vector(0, 0);
+					self.HEATReloadSupportOffsetTarget = Vector(0, 0);
+					
+					self.HEATReloadPhaseOverride = nil;
+					
+					self.HEATEnterPhaseCallbackDone = true;
+					self.HEATExitPhaseCallbackDone = true;
+					
+					self.BaseReloadTime = 0;
+				else
+					self.HEATCurrentReloadPhase = self.HEATCurrentReloadPhase + 1;
+				end
+				
+				self.HEATCurrentReloadPhaseData.exitPhaseCallback(self);
+				self.HEATCurrentReloadPhaseData = nil;
+				
+			end
+		end
+	else
+		self.BaseReloadTime = 19999;
+		
+		self.HEATReloadTimer:Reset();
+		if self.HEATReloadPhaseOnInterrupt then
+			self.HEATCurrentReloadPhase = self.HEATReloadPhaseOnInterrupt;
+			self.HEATReloadPhaseOnInterrupt = nil;
+		end
+	end
+	
+	if self:DoneReloading() == true then
+		self.HEATFireDelayTimer:Reset()
+		self.Magazine.RoundCount = self.HEATFullMagazineRoundCount;
+		if self.HEATEmptyReload and self.HEATPlusOneChamberedRound then
+			self.HEATEmptyReload = false;
+			self.Magazine.RoundCount = math.max(1, self.Magazine.RoundCount - 1);
+		end
+	end	
+
+	-- Delayed fire system
+
+	local fire = self:IsActivated() and self.RoundInMagCount > 0;
+	if self.RoundInMagCount > 0 then
+		self:Deactivate()
+	end
+
+	if self.HEATParent and self.HEATDelayedFirstShot == true then
+		
+		--if self.parent:GetController():IsState(Controller.WEAPON_FIRE) and not self:IsReloading() then
+		if fire and not self:IsReloading() then
+			if not self.Magazine or self.Magazine.RoundCount < 1 then
+				--self:Reload()
+				self:Activate()
+			elseif not self.activated and not self.HEATDelayedFire and self.HEATFireDelayTimer:IsPastSimMS(1 / (self.RateOfFire / 60) * 1000) then
+				self.HEATDelayedFireActivated = true
+				
+				self.HEATPreSound:Play(self.Pos);
+				
+				self.HEATFireDelayTimer:Reset()
+				
+				self.HEATDelayedFire = true
+				self.HEATDelayedFireTimer:Reset()
+			end
+		else
+			if self.HEATDelayedFireActivated then
+				self.HEATDelayedFireActivated = false
+			end
+		end
+	elseif fire == false then
+		self.HEATDelayedFirstShot = true;
+	end
+	
+	if self.FiredFrame then
+		self.HEATHorizontalAnim = self.HEATRecoilHorizontalAnim;
+		self.HEATFiringAnimationTimer:Reset();
+	
+		self.HEATAngVel = self.HEATAngVel - RangeRand(1 - self.HEATRecoilAngVariation / 2, 1 + self.HEATRecoilAngVariation / 2) * self.HEATRecoilAngAnim
+		
+		if self.RoundInMagCount > 0 then
+		else
+			self.HEATEmptyReload = true;
+		end
+		
+		for i = 1, 3 do
+			local Effect = CreateMOSParticle("Tiny Smoke Ball 1", "Base.rte")
+			if Effect then
+				Effect.Pos = self.MuzzlePos;
+				Effect.Vel = (self.Vel + Vector(RangeRand(-20,20), RangeRand(-20,20)) + Vector(150*self.FlipFactor,0):RadRotate(self.RotAngle)) / 30
+				MovableMan:AddParticle(Effect)
+			end
+		end
+		
+		local Effect = CreateMOSParticle("Side Thruster Blast Ball 1", "Base.rte")
+		if Effect then
+			Effect.Pos = self.MuzzlePos;
+			Effect.Vel = (self.Vel + Vector(150*self.FlipFactor,0):RadRotate(self.RotAngle)) / 10
+			MovableMan:AddParticle(Effect)
+		end
+
+		local outdoorRays = 0;
+		local indoorRays = 0;
+		local bigIndoorRays = 0;
+		
+		local rayThreshold = 2;
+
+		if self.HEATParent and self.HEATParent:IsPlayerControlled() then
+			local Vector2 = Vector(0,-700); -- straight up
+			local Vector2Left = Vector(0,-700):RadRotate(45*(math.pi/180));
+			local Vector2Right = Vector(0,-700):RadRotate(-45*(math.pi/180));			
+			local Vector2SlightLeft = Vector(0,-700):RadRotate(22.5*(math.pi/180));
+			local Vector2SlightRight = Vector(0,-700):RadRotate(-22.5*(math.pi/180));		
+			local Vector3 = Vector(0,0); -- dont need this but is needed as an arg
+			local Vector4 = Vector(0,0); -- dont need this but is needed as an arg
+
+			self.ray = SceneMan:CastObstacleRay(self.Pos, Vector2, Vector3, Vector4, self.RootID, self.Team, 128, 7);
+			self.rayRight = SceneMan:CastObstacleRay(self.Pos, Vector2Right, Vector3, Vector4, self.RootID, self.Team, 128, 7);
+			self.rayLeft = SceneMan:CastObstacleRay(self.Pos, Vector2Left, Vector3, Vector4, self.RootID, self.Team, 128, 7);			
+			self.raySlightRight = SceneMan:CastObstacleRay(self.Pos, Vector2SlightRight, Vector3, Vector4, self.RootID, self.Team, 128, 7);
+			self.raySlightLeft = SceneMan:CastObstacleRay(self.Pos, Vector2SlightLeft, Vector3, Vector4, self.RootID, self.Team, 128, 7);
+			
+			self.rayTable = {self.ray, self.rayRight, self.rayLeft, self.raySlightRight, self.raySlightLeft};
+		else
+			rayThreshold = 1; -- has to be different for AI
+			local Vector2 = Vector(0,-700); -- straight up
+			local Vector3 = Vector(0,0); -- dont need this but is needed as an arg
+			local Vector4 = Vector(0,0); -- dont need this but is needed as an arg		
+			self.ray = SceneMan:CastObstacleRay(self.Pos, Vector2, Vector3, Vector4, self.RootID, self.Team, 128, 7);
+			
+			self.rayTable = {self.ray};
+		end
+		
+		for _, rayLength in ipairs(self.rayTable) do
+			if rayLength < 0 then
+				outdoorRays = outdoorRays + 1;
+			elseif rayLength > 170 then
+				bigIndoorRays = bigIndoorRays + 1;
+			else
+				indoorRays = indoorRays + 1;
+			end
+		end
+		
+		if outdoorRays >= rayThreshold then
+			self.HEATReflectionOutdoorsSound:Play(self.Pos);
+		else
+			self.HEATReflectionIndoorsSound:Play(self.Pos);
+		end
+	end
+	
+	if self.HEATDelayedFire and self.HEATDelayedFireTimer:IsPastSimMS(self.HEATDelayedFireTimeMS) then
+		self:Activate()	
+		self.HEATDelayedFire = false
+		self.HEATDelayedFirstShot = false;
+	end
+	
+	-- Animation
+	if self.HEATParent then
+		self.HEATHorizontalAnim = math.floor(self.HEATHorizontalAnim / (1 + TimerMan.DeltaTimeSecs * 24.0) * 1000) / 1000
+		self.HEATVerticalAnim = math.floor(self.HEATVerticalAnim / (1 + TimerMan.DeltaTimeSecs * 15.0) * 1000) / 1000
+		
+		local stance = Vector()
+		stance = stance + Vector(-1,0) * self.HEATHorizontalAnim -- Horizontal animation
+		stance = stance + Vector(0,5) * self.HEATVerticalAnim -- Vertical animation
+		
+		self.HEATRotationTarget = self.HEATRotationTarget - (self.HEATAngVel * 4)
+		
+		if self.FiredFrame then
+			self.HEATRecoilStr = self.HEATRecoilStr + ((math.random(10, self.HEATRecoilRandomUpper * 10) / 10) * 0.5 * self.HEATRecoilStrength) + (self.HEATRecoilStr * 0.6 * self.HEATRecoilPowStrength)
+			self:SetNumberValue("recoilStrengthBase", self.HEATRecoilStrength * (1 + self.HEATRecoilPowStrength) / self.HEATRecoilDamping)
+		end
+		self:SetNumberValue("recoilStrengthCurrent", self.HEATRecoilStr)
+		
+		self.HEATRecoilStr = math.floor(self.HEATRecoilStr / (1 + TimerMan.DeltaTimeSecs * 8.0 * self.HEATRecoilDamping) * 1000) / 1000
+		self.HEATRecoilAcc = (self.HEATRecoilAcc + self.HEATRecoilStr * TimerMan.DeltaTimeSecs) % (math.pi * 4)
+		
+		local recoilA = (math.sin(self.HEATRecoilAcc) * self.HEATRecoilStr) * 0.05 * self.HEATRecoilStr
+		local recoilB = (math.sin(self.HEATRecoilAcc * 0.5) * self.HEATRecoilStr) * 0.01 * self.HEATRecoilStr
+		local recoilC = (math.sin(self.HEATRecoilAcc * 0.25) * self.HEATRecoilStr) * 0.05 * self.HEATRecoilStr
+		
+		local recoilFinal = math.max(math.min(recoilA + recoilB + recoilC, self.HEATRecoilMax), -self.HEATRecoilMax)
+		
+		self.SharpLength = math.max(self.HEATOriginalSharpLength - (self.HEATRecoilStr * 3 + math.abs(recoilFinal)), 0)
+		
+		self.HEATRotationTarget = self.HEATRotationTarget + recoilFinal -- apply the recoil	
+		
+		self.HEATRotation = (self.HEATRotation + self.HEATRotationTarget * TimerMan.DeltaTimeSecs * self.HEATRotationSpeed) / (1 + TimerMan.DeltaTimeSecs * self.HEATRotationSpeed)
+		self.SupportOffset = self.SupportOffset + ((self.HEATReloadSupportOffsetTarget - self.SupportOffset) * TimerMan.DeltaTimeSecs * self.HEATReloadSupportOffsetSpeed)
+		local total = math.rad(self.HEATRotation) * self.FlipFactor
+		
+		self.InheritedRotAngleOffset = total * self.FlipFactor;
+		-- self.RotAngle = self.RotAngle + total;
+		-- self:SetNumberValue("MagRotation", total);
+		
+		-- local jointOffset = Vector(self.JointOffset.X * self.FlipFactor, self.JointOffset.Y):RadRotate(self.RotAngle);
+		-- local offsetTotal = Vector(jointOffset.X, jointOffset.Y):RadRotate(-total) - jointOffset
+		-- self.Pos = self.Pos + offsetTotal;
+		-- self:SetNumberValue("MagOffsetX", offsetTotal.X);
+		-- self:SetNumberValue("MagOffsetY", offsetTotal.Y);
+		
+		if self.HEATReloadStanceOffsetTarget then
+			self.HEATReloadStanceOffset = self.HEATReloadStanceOffset + ((self.HEATReloadStanceOffsetTarget - self.HEATReloadStanceOffset) * TimerMan.DeltaTimeSecs * 2.5)
+			self.StanceOffset = Vector(self.HEATOriginalStanceOffset.X, self.HEATOriginalStanceOffset.Y) + stance + self.HEATReloadStanceOffset
+			self.SharpStanceOffset = Vector(self.HEATOriginalSharpStanceOffset.X, self.HEATOriginalSharpStanceOffset.Y) + stance + self.HEATReloadStanceOffset;
+		else
+			self.HEATReloadStanceOffset = Vector(0, 0)
+			self.StanceOffset = Vector(self.HEATOriginalStanceOffset.X, self.HEATOriginalStanceOffset.Y) + stance
+			self.SharpStanceOffset = Vector(self.HEATOriginalSharpStanceOffset.X, self.HEATOriginalSharpStanceOffset.Y) + stance
+		end
+	end
+end
---
+++ CED.rte/Devices/Weapons/Handheld/TestPistol/TestPistolHEATStats.lua
@@ -0,0 +1,203 @@
+function Create(self)
+
+	-- Firing animation system
+	self.useHEATFiringAnimation = true;
+	
+	self.HEATFiringAnimationEndFrame = 2;
+	
+	-- CompliSound atmo firing sound system
+	
+	self.HEATReflectionOutdoorsSound = CreateSoundContainer("Reflection Outdoors CED Test Pistol", "CED.rte");
+	self.HEATReflectionIndoorsSound = CreateSoundContainer("Reflection Indoors CED Test Pistol", "CED.rte");
+
+	-- Reload system
+	self.useHEATReload = true;
+	
+	self.HEATPlusOneChamberedRound = true;
+	self.HEATFullMagazineRoundCount = 10; -- This is needed to avoid painful things like having to save the magazine roundcount in case of save/loading mid-reload etc.
+	
+	self.HEATFakeMagazineMOSRotating = CreateMOSRotating("Fake Magazine MOSRotating CED Test Pistol", "CED.rte");
+	-- Ignore FlipFactor here, it is handled automatically
+	self.HEATFakeMagazineOffset = Vector(-4, 2);
+	self.HEATFakeMagazineVelocity = Vector(0.5, 2);
+	self.HEATFakeMagazineAngularVel = -1;
+	
+	-- During callbacks, you have access to self.
+	-- Notable variables:
+	-- HEATAmmoCounter
+	-- 
+	
+	self.HEATReloadPhases = {};
+	
+	local i = 1;
+	local reloadPhase = {};
+	reloadPhase.Name = "MagOut";
+	reloadPhase.removesMag = true;
+	reloadPhase.addsMag = false;
+	reloadPhase.prepareSound = CreateSoundContainer("Mag Out Prepare CED Test Pistol", "CED.rte");
+	reloadPhase.prepareDelay = 100;
+	reloadPhase.prepareSoundLength = 140;
+	reloadPhase.afterSound = CreateSoundContainer("Mag Out CED Test Pistol", "CED.rte");
+	reloadPhase.afterDelay = 200;
+	reloadPhase.reloadStanceOffsetTarget = Vector(5, -2);
+	reloadPhase.reloadSupportOffsetSpeed = 16;
+	reloadPhase.reloadSupportOffsetTarget = Vector(-4, 5)
+	reloadPhase.rotationTarget = 25;
+	reloadPhase.angVel = 2;
+	reloadPhase.horizontalAnim = 0;
+	reloadPhase.verticalAnim = 1;
+	reloadPhase.autoAnimateFrames = true;
+	reloadPhase.startFrame = 0;
+	reloadPhase.endFrame = 0;
+	reloadPhase.phaseOnInterrupt = nil;
+	reloadPhase.endIfNotEmptyReload = false;
+	reloadPhase.enterPhaseCallback = function (self)
+		
+	end
+	reloadPhase.constantCallback = function (self)
+		
+	end
+	reloadPhase.finishCallback = function (self)
+		
+	end
+	reloadPhase.exitPhaseCallback = function (self)
+		
+	end
+	
+	self.HEATReloadPhases[i] = reloadPhase;
+	
+	i = i + 1;
+	reloadPhase = {};
+	reloadPhase.Name = "MagIn";
+	reloadPhase.removesMag = false;
+	reloadPhase.addsMag = true;
+	reloadPhase.prepareSound = CreateSoundContainer("Mag In Prepare CED Test Pistol", "CED.rte");
+	reloadPhase.prepareDelay = 630;
+	reloadPhase.prepareSoundLength = 550;
+	reloadPhase.afterSound = CreateSoundContainer("Mag In CED Test Pistol", "CED.rte");
+	reloadPhase.afterDelay = 200;
+	reloadPhase.reloadStanceOffsetTarget = Vector(0, 0);
+	reloadPhase.reloadSupportOffsetSpeed = 16;
+	reloadPhase.reloadSupportOffsetTarget = Vector(-4, 8)
+	reloadPhase.rotationTarget = 20;
+	reloadPhase.angVel = -2;
+	reloadPhase.horizontalAnim = 0;
+	reloadPhase.verticalAnim = -1;
+	reloadPhase.autoAnimateFrames = true;
+	reloadPhase.startFrame = 0;
+	reloadPhase.endFrame = 0;
+	reloadPhase.phaseOnInterrupt = nil;
+	reloadPhase.endIfNotEmptyReload = true;
+	reloadPhase.enterPhaseCallback = function (self)
+		
+	end
+	reloadPhase.constantCallback = function (self)
+		
+	end
+	reloadPhase.finishCallback = function (self)
+		
+	end
+	reloadPhase.exitPhaseCallback = function (self)
+		
+	end
+	
+	self.HEATReloadPhases[i] = reloadPhase;
+	
+	i = i + 1;
+	reloadPhase = {};
+	reloadPhase.Name = "BoltBack";
+	reloadPhase.removesMag = false;
+	reloadPhase.addsMag = false;
+	reloadPhase.prepareSound = CreateSoundContainer("Bolt Back Prepare CED Test Pistol", "CED.rte");
+	reloadPhase.prepareDelay = 250;
+	reloadPhase.prepareSoundLength = 220;
+	reloadPhase.afterSound = CreateSoundContainer("Bolt Back CED Test Pistol", "CED.rte");
+	reloadPhase.afterDelay = 150;
+	reloadPhase.reloadStanceOffsetTarget = Vector(4, -2);
+	reloadPhase.reloadSupportOffsetSpeed = 16;
+	reloadPhase.reloadSupportOffsetTarget = Vector(-4, -3)
+	reloadPhase.rotationTarget = 30;
+	reloadPhase.angVel = -15;
+	reloadPhase.horizontalAnim = 0;
+	reloadPhase.verticalAnim = 0;
+	reloadPhase.autoAnimateFrames = true;
+	reloadPhase.startFrame = 0;
+	reloadPhase.endFrame = 2;
+	reloadPhase.phaseOnInterrupt = nil;
+	reloadPhase.endIfNotEmptyReload = false;
+	reloadPhase.enterPhaseCallback = function (self)
+		
+	end
+	reloadPhase.constantCallback = function (self)
+		
+	end
+	reloadPhase.finishCallback = function (self)
+		
+	end
+	reloadPhase.exitPhaseCallback = function (self)
+		
+	end
+	
+	self.HEATReloadPhases[i] = reloadPhase;
+	
+	i = i + 1;
+	reloadPhase = {};
+	reloadPhase.Name = "BoltForward";
+	reloadPhase.removesMag = false;
+	reloadPhase.addsMag = false;
+	reloadPhase.prepareSound = nil;
+	reloadPhase.prepareDelay = 175;
+	reloadPhase.prepareSoundLength = 0;
+	reloadPhase.afterSound = CreateSoundContainer("Bolt Forward CED Test Pistol", "CED.rte");
+	reloadPhase.afterDelay = 200;
+	reloadPhase.reloadStanceOffsetTarget = Vector(0, 0);
+	reloadPhase.reloadSupportOffsetSpeed = 16;
+	reloadPhase.reloadSupportOffsetTarget = Vector(-7, -2)
+	reloadPhase.rotationTarget = 30;
+	reloadPhase.angVel = 35;
+	reloadPhase.horizontalAnim = 0;
+	reloadPhase.verticalAnim = 0;
+	reloadPhase.autoAnimateFrames = true;
+	reloadPhase.startFrame = 2;
+	reloadPhase.endFrame = 0;
+	reloadPhase.phaseOnInterrupt = 3;
+	reloadPhase.endIfNotEmptyReload = false;
+	reloadPhase.enterPhaseCallback = function (self)
+		
+	end
+	reloadPhase.constantCallback = function (self)
+		
+	end
+	reloadPhase.finishCallback = function (self)
+		
+	end
+	reloadPhase.exitPhaseCallback = function (self)
+		
+	end
+	
+	self.HEATReloadPhases[i] = reloadPhase;
+
+	
+	-- Recoil system
+	self.useHEATRecoil = true;
+	
+	self.HEATRecoilHorizontalAnim = 5;
+	self.HEATRecoilAngAnim = 7;
+	self.HEATRecoilAngVariation = 0.2;
+	
+	self.HEATRecoilAcc = 0 -- for sinous
+	self.HEATRecoilStr = 0 -- for accumulator
+	self.HEATRecoilStrength = 10 -- multiplier for base recoil added to the self.recoilStr when firing
+	self.HEATRecoilPowStrength = 0.2 -- multiplier for self.recoilStr when firing
+	self.HEATRecoilRandomUpper = 2 -- upper end of random multiplier (1 is lower)
+	self.HEATRecoilDamping = 0.8
+	
+	self.HEATRecoilMax = 4 -- in deg.
+	
+	
+	-- Delayed fire system
+	self.useHEATDelayedFire = true;
+	
+	self.HEATPreSound = CreateSoundContainer("Pre CED Test Pistol", "CED.rte");
+	self.HEATDelayedFireTimeMS = 70
+end
