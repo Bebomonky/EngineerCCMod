67a1fbe81d1b3dc2a65b10f3cde7962bff171043
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2024-09-25 06:34:17 +0300

    Cooler nade stuff

--- CED.rte/Devices/Weapons/Thrown/VoConcussor/VoConcussor.lua
+++ CED.rte/Devices/Weapons/Thrown/VoConcussor/VoConcussor.lua
@@ -7,7 +7,8 @@ function Create(self)
 	self.exploIndoorsSound = CreateSoundContainer("Explo Indoors CED Vossberg Concussor", "CED.rte");
 	
 	self.exploDelay = 2500;
-	self.implosionRadius = 150;
+	self.pushRadius = 150;
+	self.pullRadius = 175;
 	self.currentRadius = 0;
 	
 	self.Timer = Timer();
@@ -18,20 +19,27 @@ end
 function Update(self)
 	self.implodeSound.Pos = self.Pos;
 	
-	PrimitiveMan:DrawCirclePrimitive(self.Pos, self.currentRadius, 13);
+	--PrimitiveMan:DrawCirclePrimitive(self.Pos, self.currentRadius, 13);
 	
 	if self.Exploding then
-		self.Vel = Vector(self.Vel.X * 0.3, self.Vel.Y * 0.3);
+		self.Vel = Vector(self.Vel.X * 0.1, self.Vel.Y * 0.1);
 	end
 
 	if self.Imploding then
-		self.currentRadius = self.implosionRadius;
+		self.currentRadius = self.pullRadius;
+		
+		CameraMan:AddScreenShake(1.5, self.Pos);
 		
 		for mo in MovableMan:GetMOsInRadius(self.Pos, self.currentRadius, -1, false) do
 			local dist = SceneMan:ShortestDistance(self.Pos, mo.Pos, SceneMan.SceneWrapsX);
 			
 			if SceneMan:CastStrengthSumRay(self.Pos, self.Pos + dist, 2, -1) < 1000 then
 				mo.Vel = mo.Vel + dist:SetMagnitude(math.max(-1, -1 / (mo.Mass / 200)));
+				
+				-- Affect it even more if it's very low mass, to manipulate smoke and such
+				if mo.Mass < 0.5 then
+					mo.Vel = mo.Vel + dist:SetMagnitude(10);
+				end
 				
 				if IsActor(mo) then
 					mo = ToActor(mo);
@@ -78,13 +86,6 @@ function Update(self)
 				self.exploIndoorsSound:Play(self.Pos);
 			end		
 			
-			local payload = CreateMOPixel("CED Vossberg Concussor Stun Particle", "CED.rte");
-			if payload then
-				payload.Pos = self.Pos;
-				MovableMan:AddParticle(payload);
-			end			
-			self:GibThis();
-			
 			for mo in MovableMan:GetMOsInRadius(self.Pos, self.currentRadius, -1, false) do
 				local dist = SceneMan:ShortestDistance(self.Pos, mo.Pos, SceneMan.SceneWrapsX);
 				if SceneMan:CastStrengthSumRay(self.Pos, self.Pos + dist, 2, -1) < 1000	then
@@ -93,15 +94,81 @@ function Update(self)
 				end
 			end			
 			
+			local payload = CreateMOPixel("CED Vossberg Concussor Stun Particle", "CED.rte");
+			payload.Pos = self.Pos;
+			MovableMan:AddParticle(payload);
+			
+			-- Ground smoke
+			local smallAmount = 50;
+			for i = 1, smallAmount do		
+				local effect = CreateMOSRotating("CompliSound Ground Smoke Particle Small", "0CompliSoundEmporium.rte")
+				effect.Pos = self.Pos;
+				effect.Vel = self.Vel + Vector(math.random(90,150),0):RadRotate(math.pi * 2 / smallAmount * i + RangeRand(-2,2) / smallAmount)
+				effect.Lifetime = effect.Lifetime * RangeRand(0.5,2.0)
+				effect.AirResistance = effect.AirResistance * RangeRand(0.5,0.8)
+				MovableMan:AddParticle(effect)
+			end
+			
+			local largeAmount = 30;
+			for i = 1, largeAmount do
+
+				local effect = CreateMOSRotating("CompliSound Ground Smoke Particle Large", "0CompliSoundEmporium.rte")
+				effect.Pos = self.Pos;
+				effect.Vel = self.Vel + Vector(math.random(90,150),0):RadRotate(math.pi * 2 / largeAmount * i + RangeRand(-2,2) / largeAmount)
+				effect.Lifetime = effect.Lifetime * RangeRand(0.5,2.0)
+				effect.AirResistance = effect.AirResistance * RangeRand(0.5,0.8)
+				MovableMan:AddParticle(effect)
+			end
+			
+			-- Furthermore, go some distance up and throw smoke downwards for a wide effect
+			
+			local freePos = Vector();
+			local ray = SceneMan:CastObstacleRay(self.Pos, Vector(0, -200), Vector(), freePos, self.ID, self.Team, 128, 5);
+			if ray < 0 or ray > 30 then
+				local pos = ray < 0 and self.Pos + Vector(0, -200) or self.Pos + Vector(0, -ray);
+				--PrimitiveMan:DrawCirclePrimitive(pos, 150, 13);
+				local smallAmount = 50;
+				for i = 1, smallAmount do		
+					local effect = CreateMOSRotating("CompliSound Ground Smoke Particle Small", "0CompliSoundEmporium.rte")
+					effect.Pos = pos;
+					effect.Vel = Vector(math.random(-90, 90), 200);
+					effect.Lifetime = effect.Lifetime * RangeRand(0.5,2.0)
+					effect.AirResistance = effect.AirResistance * RangeRand(0.5,0.8)
+					MovableMan:AddParticle(effect)
+				end
+				
+				local largeAmount = 30;
+				for i = 1, largeAmount do
+
+					local effect = CreateMOSRotating("CompliSound Ground Smoke Particle Large", "0CompliSoundEmporium.rte")
+					effect.Pos = pos;
+					effect.Vel = Vector(math.random(-90, 90), 200);
+					effect.Lifetime = effect.Lifetime * RangeRand(0.5,2.0)
+					effect.AirResistance = effect.AirResistance * RangeRand(0.5,0.8)
+					MovableMan:AddParticle(effect)
+				end
+			end
+			
+			CameraMan:AddScreenShake(35, self.Pos);
+			
+			self:GibThis();			
+			
 		end
 	elseif self.Exploding then
-		self.currentRadius = self.implosionRadius * (self.Timer.ElapsedSimTimeMS / self.pushTime);
+		self.currentRadius = self.pushRadius * (self.Timer.ElapsedSimTimeMS / self.pushTime);
+		
+		CameraMan:AddScreenShake(1, self.Pos);
 		
 		for mo in MovableMan:GetMOsInRadius(self.Pos, self.currentRadius, -1, false) do
 			local dist = SceneMan:ShortestDistance(self.Pos, mo.Pos, SceneMan.SceneWrapsX);
 		
 			if SceneMan:CastStrengthSumRay(self.Pos, self.Pos + dist, 2, -1) < 1000	then
 				mo.Vel = mo.Vel + dist:SetMagnitude(math.min(1, 1 / (mo.Mass / 200)));
+				
+				-- Affect it even more if it's very low mass, to manipulate smoke and such
+				if mo.Mass < 0.5 then
+					mo.Vel = mo.Vel + dist:SetMagnitude(10);
+				end
 				
 				if IsActor(mo) then
 					mo = ToActor(mo);
@@ -122,21 +189,17 @@ function Update(self)
 			end
 		end
 	elseif self.Active then
+	
 		if self.Timer:IsPastSimMS(self.exploDelay) then
 			self.Exploding = true;
 			self.implodeSound:Play(self.Pos);
 			self.currentRadius = 0;
 			self.Timer:Reset();
 			
-			for i = 1, 9 do
-				local Effect = CreateMOSParticle("Tiny Smoke Ball 1", "Base.rte")
-				if Effect then
-					Effect.HitsMOs = false;
-					Effect.Pos = self.Pos;
-					Effect.Vel = self.Vel;
-					MovableMan:AddParticle(Effect)
-				end
-			end
+			local smokePayload = CreateMOSRotating("CED Vossberg Concussor Smoke Payload", "CED.rte");
+			smokePayload.Pos = self.Pos;
+			MovableMan:AddParticle(smokePayload);	
+			smokePayload:GibThis();
 		end
 	elseif self.Activated then
 		if not self:IsAttached() then
