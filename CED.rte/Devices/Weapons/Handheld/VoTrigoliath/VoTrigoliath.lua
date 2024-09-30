cd7f0514777876f04332a5bffd8e9262a6a5975c
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2024-09-30 14:49:24 +0300

    Unfinished new gun, needs sprites

---
+++ CED.rte/Devices/Weapons/Handheld/VoTrigoliath/VoTrigoliath.lua
@@ -0,0 +1,207 @@
+require("/CEDSettings");
+
+function Create(self)
+	self.VoTrigoliathPreSound = CreateSoundContainer("Pre CED Vossberg Trigoliath", "CED.rte");
+	self.VoTrigoliathMultiFirePreSound = CreateSoundContainer("Multi Fire Pre CED Vossberg Trigoliath", "CED.rte");
+	
+	self.VoTrigoliathMultiFireAddSound = CreateSoundContainer("Multi Fire Add CED Vossberg Trigoliath", "CED.rte");
+	
+	self.VoTrigoliathMultiFireReflectionOutdoorsSound = CreateSoundContainer("Multi Fire Reflection Outdoors CED Vossberg Trigoliath", "CED.rte");
+	self.VoTrigoliathMultiFireReflectionIndoorsSound = CreateSoundContainer("Multi Fire Reflection Indoors CED Vossberg Trigoliath", "CED.rte");
+	
+	self.VoTrigoliathSlugsLoaded = false;
+	
+	self.VoTrigoliathCasingsToRemove = 0;
+	
+	self.VoTrigoliathMultiFireHoldTime = 200;
+	self.VoTrigoliathMultiFireTimer = Timer();
+	
+	self.VoTrigoliathMultiFireReloadableCooldown = 500;
+	
+	self.VoTrigoliathSpawnProjectileFunction = function (self, extraParticleForMultifire)
+		self.VoTrigoliathCasingsToRemove = self.VoTrigoliathCasingsToRemove + 1;
+		
+		local topBarrelVector = Vector(0, 0);
+		if self.HEATAmmoCounter == 1 then
+			topBarrelVector = Vector(0, -2):RadRotate(self.RotAngle);
+		end
+
+		if self.VoTrigoliathSlugsLoaded then
+			local velocity = 170;
+			local spread = 0;
+			
+			for i = 1, 3 + extraParticleForMultifire do
+				local shot = CreateMOPixel("Slug CED Vossberg Trigoliath", "CED.rte");
+				shot.Pos = self.MuzzlePos + topBarrelVector;
+				shot.Vel = self.Vel + Vector(velocity * self.FlipFactor, spread):RadRotate(self.RotAngle);
+				shot.Team = self.Team;
+				shot.IgnoresTeamHits = true;
+				shot:SetWhichMOToNotHit(ToMovableObject(self), 150);
+				MovableMan:AddParticle(shot);
+			end
+		else
+			local velocity = 150;
+			local spread = 2;
+			
+			for i = 1, 8 + extraParticleForMultifire do
+				local shot = CreateMOPixel("Pellet CED Vossberg Trigoliath", "CED.rte");
+				shot.Pos = self.MuzzlePos + topBarrelVector;
+				shot.Vel = self.Vel + Vector(velocity * self.FlipFactor, spread):RadRotate(self.RotAngle);
+				shot.Team = self.Team;
+				shot.IgnoresTeamHits = true;
+				shot:SetWhichMOToNotHit(ToMovableObject(self), 150);
+				MovableMan:AddParticle(shot);
+			end
+		end
+	end
+end
+
+function OnFire(self)
+	local topBarrelVector = Vector(0, 0);
+	if self.HEATAmmoCounter == 1 then
+		topBarrelVector = Vector(0, -2):RadRotate(self.RotAngle);
+	end
+
+	-- Spawn our scripted particles here, since we only want to do it once even on multifire
+	if self.VoTrigoliathSlugsLoaded then
+		local velocity = 170;
+		local spread = 0;
+	
+		local shot = CreateMOPixel("Slug CED Vossberg Trigoliath Scripted", "CED.rte");
+		shot.Pos = self.MuzzlePos + topBarrelVector;
+		shot.Vel = self.Vel + Vector(velocity * self.FlipFactor, 0):RadRotate(self.RotAngle);
+		shot.Team = self.Team;
+		shot.IgnoresTeamHits = true;
+		shot:SetWhichMOToNotHit(ToMovableObject(self), 150);
+		MovableMan:AddParticle(shot);
+	else
+		local velocity = 150;
+		local spread = 2;
+	
+		local shot = CreateMOPixel("Pellet CED Vossberg Trigoliath Scripted", "CED.rte");
+		shot.Pos = self.MuzzlePos + topBarrelVector;
+		shot.Vel = self.Vel + Vector(velocity * self.FlipFactor, 0):RadRotate(self.RotAngle);
+		shot.Team = self.Team;
+		shot.IgnoresTeamHits = true;
+		shot:SetWhichMOToNotHit(ToMovableObject(self), 150);
+		MovableMan:AddParticle(shot);
+	end
+
+	self.VoTrigoliathSpawnProjectileFunction(self, 0);
+	
+	if self.VoTrigoliathMultiFire then
+		if self.HEATAmmoCounter > 1 then
+			self.VoTrigoliathMultiFireAddSound:Play(self.Pos);
+			
+			CameraMan:AddScreenShake(6 * self.HEATAmmoCounter, self.MuzzlePos);
+			
+			for i = 1, self.HEATAmmoCounter - 1 do
+				self.VoTrigoliathSpawnProjectileFunction(self, 1);
+			end
+
+			if self.HEATCheckIfPointIsIndoors(self, self.Pos) then
+				self.VoTrigoliathMultiFireReflectionIndoorsSound:Play(self.Pos);
+			else
+				self.VoTrigoliathMultiFireReflectionOutdoorsSound:Play(self.Pos);
+			end
+		end
+		self.HEATAmmoCounter = 0;
+		if self.Magazine then
+			self.Magazine.RoundCount = 0;
+		end
+	end
+end
+
+function OnAttach(self, newParent)
+	if IsAHuman(newParent:GetRootParent()) then
+		self.parent = ToAHuman(newParent:GetRootParent());
+	end
+end
+
+function OnDetach(self)
+	self.parent = nil;
+end
+
+function OnReload(self)
+	self.VoTrigoliathMultiFire = false;
+end
+
+function ThreadedUpdate(self)
+	if self.HEATParent and self.HEATParent:IsPlayerControlled() then
+		if not self.HEATDelayedFire then
+			if UInputMan:KeyPressed(CEDSettings.WeaponAbilitySecondary) then
+				self.BaseReloadTime = 9999999;
+				self.VoTrigoliathSwitchedAmmo = true;
+				if self.VoTrigoliathSlugsLoaded then
+					self.VoTrigoliathSlugsLoaded = false;
+				else
+					self.VoTrigoliathSlugsLoaded = true;
+				end
+				self:Reload();
+				self.VoTrigoliathCasingsToRemove = self.VoTrigoliathCasingsToRemove + self.HEATAmmoCounter;
+				self.HEATAmmoCounter = 0;
+			end
+		end
+	end
+	
+	if self.parent then
+		if (self.parent.EquippedItem and self.parent.EquippedItem.UniqueID == self.UniqueID and not self.parent.EquippedBGItem) or (self.parent.EquippedBGItem and self.parent.EquippedBGItem.UniqueID == self.UniqueID and not self.parent.EquippedItem) then
+			local fire = self.RoundInMagCount > 0 and self:IsActivated();
+			self:Deactivate();
+			
+			if fire and not self.VoTrigoliathMultiFire then
+				self.VoTrigoliathActivated = true;
+				if self.VoTrigoliathMultiFireTimer:IsPastSimMS(self.VoTrigoliathMultiFireHoldTime) then
+					if self.RoundInMagCount > 1 then
+						self.VoTrigoliathMultiFire = true;
+						self.Reloadable = false;
+						self.VoTrigoliathMultiFireTimer:Reset();
+					else
+						self:Activate();
+						self.HEATDelayedFireTimeMS = 110;
+						self.HEATPreSound = self.VoTrigoliathPreSound;
+						self.VoTrigoliathActivated = false;
+					end
+				end
+			else
+				if self.VoTrigoliathActivated then
+					self.VoTrigoliathActivated = false;
+					self:Activate();
+					if self.VoTrigoliathMultiFire then
+						self.HEATRecoilStrength = 50;
+						self.HEATRecoilDamping = 0.5;
+						self.HEATRecoilMax = 12;
+						self.HEATDelayedFireTimeMS = 140;
+						self.HEATPreSound = self.VoTrigoliathMultiFirePreSound;
+						
+						self.HEATParticleUtilityFiringSmokeDataTable.Power = 70;
+						self.HEATParticleUtilityFiringSmokeDataTable.Spread = 25;
+						self.HEATParticleUtilityFiringSmokeDataTable.SmokeMult = 1.0;
+						self.HEATParticleUtilityFiringSmokeDataTable.ExploMult = 1.0;
+						self.HEATParticleUtilityFiringSmokeDataTable.WidthSpread = 4;
+						self.HEATParticleUtilityFiringSmokeDataTable.VelocityMult = 0.5;
+					else
+						self.HEATRecoilStrength = 29;
+						self.HEATRecoilDamping = 0.6;
+						self.HEATRecoilMax = 4;
+						self.HEATDelayedFireTimeMS = 110;
+						self.HEATPreSound = self.VoTrigoliathPreSound;
+						
+						self.HEATParticleUtilityFiringSmokeDataTable.Power = 45;
+						self.HEATParticleUtilityFiringSmokeDataTable.Spread = 50;
+						self.HEATParticleUtilityFiringSmokeDataTable.SmokeMult = 0.7;
+						self.HEATParticleUtilityFiringSmokeDataTable.ExploMult = 1.0;
+						self.HEATParticleUtilityFiringSmokeDataTable.WidthSpread = 2;
+						self.HEATParticleUtilityFiringSmokeDataTable.VelocityMult = 0.3;
+					end
+				end
+				if self.Reloadable then
+					self.VoTrigoliathMultiFireTimer:Reset();
+				elseif self.VoTrigoliathMultiFireTimer:IsPastSimMS(self.VoTrigoliathMultiFireReloadableCooldown) then
+					self.Reloadable = true;
+				end
+			end
+		end
+	end
+	
+end
---
+++ CED.rte/Devices/Weapons/Handheld/VoTrigoliath/VoTrigoliathHEATStats.lua
@@ -0,0 +1,409 @@
+function Create(self)
+
+	-----------------
+	----------------- HEAT system stats file
+	-----------------
+	
+	-- This is a bunch of stats defining how your gun behaves in the HEAT system.
+	-- There are very many variables and complex behaviors, so a monolithic instruction set is difficult to make.
+	-- Instead, all variables are commented.
+	
+	-- Vanilla-equivalent reloads, such as basic shotgun-style reloads and magazine feds, should be doable without any callback coding.
+	-- For anything more complicated, you've been provided callbacks at various points, and the HEATSystem file has all relevant variables
+	-- commented such that you can change them for custom behavior. Don't be afraid to mess with them all to achieve what you need.
+	-- However, the HEATSystem file should not be changed.
+	
+	-----------------
+	----------------- General
+	-----------------
+	
+	-- Whether to enable verbose console logging or not.
+	self.HEATVerboseLogging = true;
+	
+	-- Easing functions. They have to be here so they're defined by the time you use them in reload phases.
+	self.HEATEaseLinear = function (x)
+		return x;
+	end
+	self.HEATEaseOutCubic = function (x)
+		return 1 - math.pow(1 - x, 3);
+	end
+	self.HEATEaseInOutCubic = function (x)
+		return x < 0.5 and 4 * x * x * x or 1 - math.pow(-2 * x + 2, 3) / 2;
+	end
+	self.HEATEaseInCirc = function (x)
+		return 1 - math.sqrt(1 - math.pow(x, 2));
+	end
+	
+	-- Callback when firing.
+	self.HEATFireCallback = function (self)
+	
+	end
+	
+	-- Callback when a CC reload is finished, after variables are set.
+	self.HEATDoneReloadingCallback = function (self)
+
+	end
+
+	-----------------
+	----------------- Firing animation system
+	-----------------
+	
+	-- Disable or enable the firing animation system.
+	self.useHEATFiringAnimation = false;
+	
+	-- Final frame of the firing animation. Start frame is always 0.
+	self.HEATFiringAnimationEndFrame = 0;
+	-- Whether to set the PersistentFrame to the above EndFrame on the final shot of a magazine.
+	-- Note that this can happily be overriden later by your callbacks.
+	self.HEATLockBackOnEmpty = false;
+	
+	-----------------
+	----------------- ParticleUtility firing smoke
+	-----------------
+	
+	-- Disable or enable using the ParticleUtility for firing smoke FX.
+	self.useHEATParticleUtilityFiringSmoke = true;
+	
+	-- Data to feed into the ParticleUtility. Read the ParticleUtility itself for information on these properties.
+	self.HEATParticleUtilityFiringSmokeDataTable = {};
+	self.HEATParticleUtilityFiringSmokeDataTable.Power = 45;
+	self.HEATParticleUtilityFiringSmokeDataTable.Spread = 50;
+	self.HEATParticleUtilityFiringSmokeDataTable.SmokeMult = 0.7;
+	self.HEATParticleUtilityFiringSmokeDataTable.ExploMult = 1.0;
+	self.HEATParticleUtilityFiringSmokeDataTable.WidthSpread = 2;
+	self.HEATParticleUtilityFiringSmokeDataTable.VelocityMult = 0.3;
+	self.HEATParticleUtilityFiringSmokeDataTable.LingerMult = 2.3;
+	self.HEATParticleUtilityFiringSmokeDataTable.AirResistanceMult = 2.0;
+	self.HEATParticleUtilityFiringSmokeDataTable.GravMult = 1;
+	-----------------
+	----------------- CompliSound firing sound system
+	-----------------
+	
+	-- Disable or enable the CompliSound system.
+	self.useHEATCompliSound = true;
+	
+	-- CreateSoundContainer for your outdoors tail sound.
+	self.HEATReflectionOutdoorsSound = CreateSoundContainer("Reflection Outdoors CED Vossberg Trigoliath", "CED.rte");
+	-- CreateSoundContainer for your indoors tail sound.
+	self.HEATReflectionIndoorsSound = CreateSoundContainer("Reflection Indoors CED Vossberg Trigoliath", "CED.rte");
+
+	-----------------
+	----------------- Staged reload system
+	-----------------
+	
+	-- Disable or enable the entire staged reload system.
+	self.useHEATReload = true;
+	
+	-- Whether to take away one round from the final reload when reloading from empty.
+	self.HEATPlusOneChamberedRound = false;
+	-- Your full magazine size, including the one in the chamber. This should be equal to the Magazine's RoundCount.
+	self.HEATFullMagazineRoundCount = 3;
+	
+	-- Whether this becomes dual-reloadable when not emptied. Won't have an effect if you don't expect this to ever be dual-wielded.
+	-- If you don't want the HEATSystem to meddle with this at all, leave it false.
+	self.HEATDualReloadableIfNotEmpty = false;	
+	
+	-- Whether to trigger the reload staging after every shot, for pump-actions, bolt-actions, etcetera.
+	self.HEATStageAfterEveryShot = false;
+	-- Phase to go to if the above is true, and triggered during regular gunfire where the gun isn't emptied.
+	-- Useful to skip your reloading first phase to go to, for example, a pumping second and third phase.
+	self.HEATPhaseAfterFiringIfNotReloading = 0;
+	
+	-- Override for the ReloadTime when reloading with rounds still in the magazine.
+	-- Autocalculated using endsIfNotEmptyReload if nil here. Relevant only for the progress bar.
+	-- Make sure not to set these two variables lower than the actual time that will be taken or it will end the reload prematurely
+	-- and break things.
+	self.HEATTotalFullReloadTimeOverride = 19999;
+	-- Override for the ReloadTime when reloading from empty.
+	-- Autocalculated using all phases if nil here. Relevant only for the progress bar.
+	self.HEATTotalEmptyReloadTimeOverride = 19999;
+	
+	-- Casing object to spawn on phases with spawnCasing.
+	self.HEATCasing = CreateAEmitter("Casing CED Vossberg Trigoliath", "CED.rte");
+	-- Position to spawn the casing at. Basically EjectionOffset. If nil here, will indeed use EjectionOffset. Don't include FlipFactor.
+	self.HEATCasingOffset = nil;
+	-- Velocity with which to spawn the casing.  Don't include FlipFactor.
+	self.HEATCasingVelocity = Vector(-3, -1);
+	
+	-- MOSRotating object to spawn on phases with removesMag.
+	self.HEATFakeMagazineMOSRotating = nil;
+	-- Position to spawn the object at.  Don't include FlipFactor.
+	self.HEATFakeMagazineOffset = Vector(0, 0);
+	-- Velocity with which to spawn the object.  Don't include FlipFactor.
+	self.HEATFakeMagazineVelocity = Vector(0, 0);
+	-- AngularVel to spawn the object with.  Don't include FlipFactor.
+	self.HEATFakeMagazineAngularVel = 0;
+	
+	self.HEATReloadPhases = {};
+	
+	------------------------------------------------------------------------------
+	
+	local i = 1;
+	local reloadPhase = {};
+	-- Name of the reloadPhase. Used for organization only.
+	reloadPhase.Name = "Open";
+	-- Whether to remove the FakeMag and spawn a fake magazine object on this phase.
+	reloadPhase.removesMag = false;
+	-- Whether to trigger the FakeMag to be visible on the gun again this phase.
+	reloadPhase.addsMag = false;
+	-- Whether to consider this phase finished and progress to the next one (or a specified phase number)
+	-- if the reload was interrupted after passing prepareDelay.
+	reloadPhase.autoProgressIfFinishedButInterrupted = false;
+	-- SoundContainer to play while preparing to finish this phase.
+	reloadPhase.prepareSound = nil;
+	-- Time it takes to finish this phase.
+	reloadPhase.prepareDelay = 500;
+	-- Time before finishing that the prepareSound will play. You can line up short prepareSounds with long prepareDelays
+	-- this way.
+	reloadPhase.prepareSoundLength = 0;
+	-- Sound upon finishing the phase.
+	reloadPhase.afterSound = CreateSoundContainer("Open CED Vossberg Trigoliath", "CED.rte");
+	-- Time after finishing the phase before the reload is progressed.
+	reloadPhase.afterDelay = 400;
+	-- Absolute StanceOffset to set when in this phase.
+	reloadPhase.reloadStanceOffsetTarget = Vector(-1, -1);
+	-- Speed at which SupportOffset moves when in this phase.
+	reloadPhase.reloadSupportOffsetSpeed = 10;
+	-- Absolute SupportOffset to set when in this phase. Note that low Speed can make this not be reached within the phase's lifetime.
+	reloadPhase.reloadSupportOffsetTarget = Vector(2, 2)
+	-- Rotation to set in this phase.
+	reloadPhase.rotationTarget = 20;
+	-- Strength of the rotational "kick" animation to do when this phase is finished.
+	reloadPhase.angVel = 0;
+	-- Strength of the horizontal "kick" animation to do when this phase is finished.
+	reloadPhase.horizontalAnim = 0;
+	-- Strength of the vertical "kick" animation to do when this phase is finished.
+	reloadPhase.verticalAnim = 0;
+	-- Whether to animate between the frames specified below, between this phase finishing and exiting.
+	reloadPhase.autoAnimateFrames = true;
+	-- Start frame of the auto animation.
+	reloadPhase.startFrame = 0;
+	-- End frame of the auto animation.
+	reloadPhase.endFrame = 3;
+	-- Whether to set the PersistentFrame to the endFrame above, which will persist even outside reloads until cleared by a finished reload.
+	reloadPhase.setEndFrameAsPersistent = true;
+	-- Easing function to use. You could define your own here if you really wanted.
+	reloadPhase.easingFunction = self.HEATEaseLinear;
+	-- Phase to restart the reload from if this phase is interrupted at any point.
+	reloadPhase.phaseOnInterrupt = nil;
+	-- Whether the reload ends at this phase, instead of progressing, if there were still rounds left in the magazine before a reload.
+	reloadPhase.endIfNotEmptyReload = false;
+	-- Whether this phase is a shotgun-style, looping, one-round-at-a-time reload.
+	-- This will also trigger shotgun ammo counting and setting behavior and makes PlusOneChamberedRound irrelevant.
+	reloadPhase.shotgunReloadLoop = false;
+	-- Whether this phase spawns a casing when finished.
+	reloadPhase.spawnCasing = false;
+	-- Callback after this phase is entered and all default values are set.
+	reloadPhase.enterPhaseCallback = function (self)
+
+	end
+	-- Callback done every frame of the reload, after value setting but before finish-specific behavior.
+	reloadPhase.constantCallback = function (self)
+		
+	end
+	-- Callback once this phase is finished.
+	reloadPhase.finishCallback = function (self)
+
+	end
+	-- Callback just before exiting the phase and deleting current phase data.
+	reloadPhase.exitPhaseCallback = function (self)
+		if self.VoTrigoliathSwitchedAmmo then
+			self.VoTrigoliathSwitchedAmmo = false;
+		end		
+	end
+	
+	self.HEATReloadPhases[i] = reloadPhase;
+	
+	------------------------------------------------------------------------------	
+	
+	i = i + 1;
+	reloadPhase = {};
+	reloadPhase.Name = "RoundOut";
+	reloadPhase.removesMag = false;
+	reloadPhase.addsMag = false;
+	reloadPhase.autoProgressIfFinishedButInterrupted = true;
+	reloadPhase.prepareSound = CreateSoundContainer("Round Out Prepare CED Vossberg Trigoliath", "CED.rte");
+	reloadPhase.prepareDelay = 300;
+	reloadPhase.prepareSoundLength = 200;
+	reloadPhase.afterSound = CreateSoundContainer("Round Out CED Vossberg Trigoliath", "CED.rte");
+	reloadPhase.afterDelay = 700;
+	reloadPhase.reloadStanceOffsetTarget = Vector(0, 0);
+	reloadPhase.reloadSupportOffsetSpeed = 6;
+	reloadPhase.reloadSupportOffsetTarget = Vector(2, 2)
+	reloadPhase.rotationTarget = 20;
+	reloadPhase.angVel = 0;
+	reloadPhase.horizontalAnim = 0;
+	reloadPhase.verticalAnim = 0;
+	reloadPhase.autoAnimateFrames = true;
+	reloadPhase.startFrame = 3;
+	reloadPhase.endFrame = 3;
+	reloadPhase.setEndFrameAsPersistent = true;
+	reloadPhase.easingFunction = self.HEATEaseOutCubic;
+	reloadPhase.phaseOnInterrupt = nil;
+	reloadPhase.endIfNotEmptyReload = false;
+	reloadPhase.shotgunReloadLoop = false;
+	reloadPhase.spawnCasing = false;
+	reloadPhase.enterPhaseCallback = function (self)
+
+	end
+	reloadPhase.constantCallback = function (self)
+
+	end
+	reloadPhase.finishCallback = function (self)
+		for i = 1, self.VoTrigoliathCasingsToRemove do
+			local casing;
+			casing = self.HEATCasing:Clone();
+			casing.Pos = self.EjectionPos;
+			casing.Vel = self.Vel + Vector(self.HEATCasingVelocity.X * self.FlipFactor, self.HEATCasingVelocity.Y):RadRotate(self.RotAngle);
+			casing.RotAngle = self.RotAngle;
+			casing.HFlipped = self.HFlipped;
+			MovableMan:AddParticle(casing);
+		end
+		self.VoTrigoliathCasingsToRemove = 0;
+	end
+	reloadPhase.exitPhaseCallback = function (self)
+		if self.VoTrigoliathSwitchedAmmo then
+			self.VoTrigoliathSwitchedAmmo = false;
+		end
+	end
+	
+	self.HEATReloadPhases[i] = reloadPhase;
+	
+	------------------------------------------------------------------------------	
+	
+	i = i + 1;
+	reloadPhase = {};
+	reloadPhase.Name = "RoundIn";
+	reloadPhase.removesMag = false;
+	reloadPhase.addsMag = false;
+	reloadPhase.autoProgressIfFinishedButInterrupted = false;
+	reloadPhase.prepareSound = CreateSoundContainer("Round In Prepare CED Vossberg Trigoliath", "CED.rte");
+	reloadPhase.prepareDelay = 200;
+	reloadPhase.prepareSoundLength = 180;
+	reloadPhase.afterSound = CreateSoundContainer("Round In CED Vossberg Trigoliath", "CED.rte");
+	reloadPhase.afterDelay = 380;
+	reloadPhase.reloadStanceOffsetTarget = Vector(0, 0);
+	reloadPhase.reloadSupportOffsetSpeed = 16;
+	reloadPhase.reloadSupportOffsetTarget = Vector(-4, 5)
+	reloadPhase.rotationTarget = 15;
+	reloadPhase.angVel = 0;
+	reloadPhase.horizontalAnim = 1;
+	reloadPhase.verticalAnim = -1;
+	reloadPhase.autoAnimateFrames = true;
+	reloadPhase.startFrame = 3;
+	reloadPhase.endFrame = 3;
+	reloadPhase.setEndFrameAsPersistent = false;
+	reloadPhase.easingFunction = self.HEATEaseLinear;
+	reloadPhase.phaseOnInterrupt = nil;
+	reloadPhase.endIfNotEmptyReload = false;
+	reloadPhase.shotgunReloadLoop = true;
+	reloadPhase.spawnCasing = false;
+	reloadPhase.enterPhaseCallback = function (self)
+		self.HEATReloadSupportOffsetTarget.Y = 5;
+	end
+	reloadPhase.constantCallback = function (self)
+		if self.VoTrigoliathSwitchedAmmo then
+			self.VoTrigoliathSwitchedAmmo = false;
+			self.HEATReloadPhaseOverride = 2;
+			self.VoTrigoliathCasingsToRemove = self.HEATAmmoCounter;
+		end
+	end
+	reloadPhase.finishCallback = function (self)
+
+	end
+	reloadPhase.exitPhaseCallback = function (self)
+
+	end
+	
+	self.HEATReloadPhases[i] = reloadPhase;
+	
+	------------------------------------------------------------------------------	
+	
+	i = i + 1;
+	reloadPhase = {};
+	reloadPhase.Name = "Close";
+	reloadPhase.removesMag = false;
+	reloadPhase.addsMag = false;
+	reloadPhase.autoProgressIfFinishedButInterrupted = false;
+	reloadPhase.prepareSound = CreateSoundContainer("Close Prepare CED Vossberg Trigoliath", "CED.rte");
+	reloadPhase.prepareDelay = 230;
+	reloadPhase.prepareSoundLength = 230;
+	reloadPhase.afterSound = CreateSoundContainer("Close CED Vossberg Trigoliath", "CED.rte");
+	reloadPhase.afterDelay = 600;
+	reloadPhase.reloadStanceOffsetTarget = Vector(0, 0);
+	reloadPhase.reloadSupportOffsetSpeed = 7;
+	reloadPhase.reloadSupportOffsetTarget = Vector(-2, 2)
+	reloadPhase.rotationTarget = -5;
+	reloadPhase.angVel = 0;
+	reloadPhase.horizontalAnim = 0;
+	reloadPhase.verticalAnim = 0;
+	reloadPhase.autoAnimateFrames = true;
+	reloadPhase.startFrame = 3;
+	reloadPhase.endFrame = 0;
+	reloadPhase.setEndFrameAsPersistent = true;
+	reloadPhase.easingFunction = self.HEATEaseOutCubic;
+	reloadPhase.phaseOnInterrupt = nil;
+	reloadPhase.endIfNotEmptyReload = false;
+	reloadPhase.shotgunReloadLoop = false;
+	reloadPhase.spawnCasing = false;
+	reloadPhase.enterPhaseCallback = function (self)
+	
+	end
+	reloadPhase.constantCallback = function (self)
+		if self.VoTrigoliathSwitchedAmmo then
+			self.VoTrigoliathSwitchedAmmo = false;
+			self.HEATReloadPhaseOverride = 1;
+		end
+	end
+	reloadPhase.finishCallback = function (self)
+	
+	end
+	reloadPhase.exitPhaseCallback = function (self)
+
+	end
+
+	self.HEATReloadPhases[i] = reloadPhase;
+	
+	------------------------------------------------------------------------------	
+	
+	-----------------
+	----------------- Recoil system
+	-----------------
+	
+	-- Whether to use the recoil system.
+	self.useHEATRecoil = true;
+	
+	-- Strength of the horizontal "kick" animation when firing.
+	self.HEATRecoilHorizontalAnim = 5;
+	-- Strength of the rotational "kick" animation when firing.
+	self.HEATRecoilAngAnim = 10;
+	-- Variative multiplier for the rotational kick animation. A value of "0.1" here would give you anywhere from x0.95 to x1.05 the AngAnim.
+	self.HEATRecoilAngVariation = 0.3;
+	
+	-- Strength of the recoil when firing. Affects rotation and SharpLength kickback.
+	self.HEATRecoilStrength = 29
+	-- Some sort of mathemagical strength value to affect the recoil.
+	self.HEATRecoilPowStrength = 0.2
+	-- Upper end of a random multiplier applied to the recoil. 1 is the lower end.
+	self.HEATRecoilRandomUpper = 1.1
+	-- Damping effect on the recoil - how fast it returns to normal.
+	self.HEATRecoilDamping = 0.6
+	
+	-- Maximum rotation in degrees the recoil can cause.
+	self.HEATRecoilMax = 4
+	-- Maximum low value for SharpLength as a multiplier.
+	self.HEATSharpLengthMinimumMult = 0.2;
+	
+	
+	-----------------
+	----------------- Delayed fire system
+	-----------------
+	
+	-- Whether to enable the delayed fire system or not.
+	self.useHEATDelayedFire = true;
+	
+	-- The sound to play when being activated, before firing.
+	self.HEATPreSound = nil;
+	-- Delay between being activated and firing.
+	self.HEATDelayedFireTimeMS = 110;
+end
